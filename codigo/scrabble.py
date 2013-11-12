37c7af319e8b2755683ff29079deb01ad5a7a15d
Jose Montenegro <josmontenegro2012@gmail.com>
2013-11-12 16:20:12 -0430

    Modificado el codigo de scrabble usando string.uppercase

--- codigo/scrabble.py
+++ codigo/scrabble.py
@@ -1,6 +1,7 @@
 import sys
+import string
 
-letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+letters = string.uppercase
 d = open("sowpods.txt", "r")
 for line in d:
    cur = 0
@@ -8,7 +9,7 @@ for line in d:
       for ch in line:
          if cur > 0:
             if ch in letters and ch == line[cur - 1]:
-              letters.remove(ch)
+              letters = letters.replace(ch,"")
          cur = cur + 1
 
 for a in letters:
